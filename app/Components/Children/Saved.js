d1e354cba6acb27efed7fb4acc1463ec80bfb8b7
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-08-06 17:05:53 -0400

    directory structure and some initial scaffolding

---
+++ app/Components/Children/Saved.js
@@ -0,0 +1,25 @@
+// Include React
+var React = require('react');
+
+// This is the results component
+var Saved = React.createClass({
+	render: function(){
+
+		return(
+
+			<div className="panel panel-default">
+				<div className="panel-heading">
+					<h3 className="panel-title text-center">Saved Articles</h3>
+				</div>
+				<div className="panel-body text-center">
+
+						<h1>?</h1>
+
+				</div>
+			</div>
+
+		)
+	}
+});
+
+module.exports = Saved;
---
+++ app/Components/utils/helpers.js
@@ -0,0 +1,9 @@
+// axios package for performing HTTP requests (promise based alternative to request)
+var axios = require('axios');
+
+// HELPER functions:
+var helpers = {
+
+}
+
+module.exports = helpers;
